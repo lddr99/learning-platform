e85b99fa4466688cbbefd67a03c0df2efc8ae79d
DaHung <lddr99@gmail.com>
2020-02-26 01:33:44 +0800

    filter user courses by the category

--- app/api/v1/courses.rb
+++ app/api/v1/courses.rb
@@ -6,11 +6,19 @@ module V1
       authenticate!
     end
 
+    params do
+      optional :category_ids, type: Array
+    end
+
     resource 'courses' do
       get do
         course_ids = current_user.subscription.pluck('course_id').uniq!
         courses = Course.includes(:category, price: :currency)
                         .where(id: course_ids)
+
+        unless params['category_ids'].nil?
+          courses = courses.where(category_id: params['category_ids'])
+        end
 
         Entities::CourseEntity.represent(courses)
       end

fa0054aa796539577be175e07b0bd6643fdb96d7
DaHung <lddr99@gmail.com>
2020-02-27 03:55:08 +0800

    Test course entity structure

--- spec/api/admin/course_editor_spec.rb
+++ spec/api/admin/course_editor_spec.rb
@@ -24,4 +24,30 @@ describe 'test the APIs for course editor', type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  context 'courses resource' do
+    before do
+      user = create(:admin_user)
+      sign_in user
+    end
+
+    it 'return course entities' do
+      course = create(:course)
+
+      get '/api/admin/courses'
+
+      json_response = JSON.parse(response.body).first
+
+      expect(json_response['title']).to eq(course.title)
+      expect(json_response['is_available']).to eq(course.is_available)
+      expect(json_response['duration_of_days']).to eq(course.duration_of_days)
+      expect(json_response['description']).to eq(course.description)
+      expect(json_response['url']).to eq(course.url)
+
+      expect(json_response['category_id']).to eq(course.category_id)
+
+      expect(json_response['price']['amount']).to eq(course.price.amount.to_s)
+      expect(json_response['price']['currency_id']).to eq(course.price.currency_id)
+    end
+  end
 end

15d670a5950a26b5b740b22ad9aafd4b5a497a57
DaHung <lddr99@gmail.com>
2020-02-26 19:55:35 +0800

    Add admin permission

--- app/api/api_helpers/auth_helpers.rb
+++ app/api/api_helpers/auth_helpers.rb
@@ -10,5 +10,10 @@ module APIHelpers
     def authenticate!
       error!('401 Unauthorized', 401) unless current_user
     end
+
+    def authenticate_admin!
+      authenticate!
+      error!('403 Unauthenticated', 403) unless current_user.is_admin?
+    end
   end
 end

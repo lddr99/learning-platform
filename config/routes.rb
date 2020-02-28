7d1df4bb8a9bb794f8b60edfe0bea7fd47fdbf54
DaHung <lddr99@gmail.com>
2020-02-28 17:49:19 +0800

    Update auth api route
    - /auth-api -> /api/v1/auth/

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'management/course-editor', to: 'management#course_editor'
 
-  mount_devise_token_auth_for 'User', at: 'api-auth'
+  mount_devise_token_auth_for 'User', at: '/api/v1/auth'
   mount V1::API => '/api/v1'
   mount Admin::API => '/api/admin'
 end
